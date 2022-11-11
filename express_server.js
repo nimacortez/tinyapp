274d1f9f9b81fc528c9f5ef99b9545d448804e68
Nima Cortez <cortez.nima@gmail.com>
2022-11-11 22:23:01 +0000

    added redirects from short URL to long URL

--- express_server.js
+++ express_server.js
@@ -2,14 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//ejs
 app.set("view engine", "ejs");
 
+const generateRandomString = () => {
+  const alphaNumerical = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += alphaNumerical.charAt(Math.floor(Math.random() * alphaNumerical.length));
+  }
+  return result;
+};
+
+
+//database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.use(express.urlencoded({ extended: true }));
+
+//GET
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,11 +41,15 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
+//main page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+
+//new URLs
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -41,9 +59,18 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars)
 });
 
+//POST
+
+//random string and add urls to database
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  console.log(req.body, shortURL); // Log the POST request body to the console
+  urlDatabase[shortURL] =  req.body.longURL;
+  res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
 });
 
-function generateRandomString() {}
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
